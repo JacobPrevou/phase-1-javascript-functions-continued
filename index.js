1aa63f99f5818c723c3880c7e22de545a373b770
Jake <prevou15@gmail.com>
2021-09-16 06:54:47 -0500

    commit tree d83f2ed039908a557fcad0e293e0a4a3106e8255

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork("work from home");
+
+
+function wrapAdjective(emphatic = '*'){
+    const inner = function (byDefault = "special") {
+        return `You are ${emphatic}${byDefault}${emphatic}!`;
+    }
+    return inner;
+}
